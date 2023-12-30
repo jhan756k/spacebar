374453fbdda4b62bc99daaf38c1943ba8b4992a8
Jooney <jhan756k@gmail.com>
2023-12-30 22:06:48 +0900

    🔥 FEAT: Setting & Header

---
+++ src/Components/Header.tsx
@@ -0,0 +1,39 @@
+import "../Style/Header.css";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="wrap">
+        <div className="itemlist">
+          <div className="logodiv">
+            <a href="/" className="logoimg">
+              <img
+                src="src\Assets\spacebar.png"
+                alt="monkey"
+                className="logoitem"
+              />
+            </a>
+          </div>
+
+          <div className="pagediv">
+            <a href="/" className="pdivitem">
+              <i className="bi-insta" />
+              space_bar
+            </a>
+            <a href="/" className="pdivitem">
+              Home
+            </a>
+            <a href="/" className="pdivitem">
+              Team
+            </a>
+            <a href="/" className="pdivitem">
+              About
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/Page/HomePage.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../Style/HomePage.css';
+import Header from '../Components/Header';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+      <div className="background">
+        <div className="title">
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
