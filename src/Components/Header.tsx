a04feb1b24a9f306a81217b5f1e57bae03da98d5
Jooney <jhan756k@gmail.com>
2023-12-31 16:18:11 +0900

    🔥 FEAT: Mobile hamburger menu & fixes

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import "../Style/Header.css";
+import { Squash as Hamburger } from "hamburger-react";
 
 const Header = () => {
+  const [hamburgerOpen, setHamburgerOpen] = useState(false);
+
   return (
     <div>
       <div className="wrap">
@@ -14,7 +18,6 @@ const Header = () => {
               />
             </a>
           </div>
-
           <div className="pagediv">
             <a href="/" className="pdivitem">
               <i className="bi-insta" />
@@ -32,6 +35,48 @@ const Header = () => {
             <a href="/" className="pdivitem">
               Blog
             </a>
+          </div>
+          <div className="hamburgerdiv">
+            <Hamburger
+              toggled={hamburgerOpen}
+              toggle={setHamburgerOpen}
+              color={hamburgerOpen ? "#000000" : "#ffffff"}
+            />
+          </div>
+        </div>
+        {hamburgerOpen && <div className="burgerblack" />}
+        <div
+          className="hamburgermenu"
+          style={{
+            visibility: hamburgerOpen ? "visible" : "hidden",
+            transform: hamburgerOpen ? "translateX(0)" : "translateX(100%)",
+            transition: "all 0.4s cubic-bezier(0.68, -0.55, 0.265, 1.55)",
+          }}
+        >
+          <div className="hamburgermenulist">
+            <div className="paneltitle">
+              Menu
+            </div>
+            <div className="hamburgermenuitem">
+              <a href="/" className="hamburgermenuitem">
+                Home
+              </a>
+            </div>
+            <div className="hamburgermenuitem">
+              <a href="/" className="hamburgermenuitem">
+                Team
+              </a>
+            </div>
+            <div className="hamburgermenuitem">
+              <a href="/" className="hamburgermenuitem">
+                About
+              </a>
+            </div>
+            <div className="hamburgermenuitem">
+              <a href="/" className="hamburgermenuitem">
+                Blog
+              </a>
+            </div>
           </div>
         </div>
       </div>
