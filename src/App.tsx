0caf15a22a14aedf2b98dbbff6d1bbb11f37ca33
Jooney <jhan756k@gmail.com>
2023-12-31 21:52:45 +0900

    ⚡️ ADD: Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import HomePage from './Page/HomePage'
+import HomePage from "./Page/HomePage";
+import TeamPage from "./Page/TeamPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
-    <div>
-      <HomePage />
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/team" element={<TeamPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
