9f7b1dbfed79b7fa6fb743a896847d2ceb476975
jhan756k <jhan756k@gmail.com>
2024-01-05 14:51:57 +0900

    :construction: TEST: Ubuntu push

--- src/Page/TeamPage.tsx
+++ src/Page/TeamPage.tsx
@@ -1,12 +1,14 @@
 /** @jsxImportSource @emotion/react */
 import Header from "../Components/Header";
 import TeamPageStyle from "../Style/TeamPage";
+import Footer from "../Components/Footer.tsx";
 
 const TeamPage = () => {
   return (
     <div>
       <div css={TeamPageStyle.teamDivStyle}>
         <Header />
+        <Footer />
       </div>
     </div>
   );
